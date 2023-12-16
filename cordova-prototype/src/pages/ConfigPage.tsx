eec554a9d4569e7b8e92d9a1098c58b65db76f8c
cathiecode <cathiecode@gmail.com>
2023-12-16 21:30:04 +0900

    fix: Configpage device modal

--- cordova-prototype/src/pages/ConfigPage.tsx
+++ cordova-prototype/src/pages/ConfigPage.tsx
@@ -4,6 +4,10 @@ import { startCooling } from "../features/futokoro/device";
 import Card from "../features/ui/Card";
 import Button from "../features/ui/Button";
 import { css } from "@emotion/react";
+import { useBoolean } from "usehooks-ts";
+import Modal from "../features/ui/Modal";
+import useStep from "../utils/useStep";
+import toast from "react-hot-toast";
 
 const cardTitle = css`
   margin-top: 20rem;
@@ -28,12 +32,83 @@ const debugGridButton = css`
   margin: auto;
 `;
 
+const modalTitle = css`
+  color: #8b8b8b;
+  text-align: center;
+  font-size: 24rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const modalDescription = css`
+  color: #000;
+  margin-top: 14rem;
+  font-size: 16rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const modalHint = css`
+  color: #000;
+  font-size: 12rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-align: center;
+  margin-top: 20rem;
+`;
+
+const modalSuccess = css`
+  margin-top: 32rem;
+  color: #000;
+  text-align: center;
+  font-size: 24rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const link = css`
+  color: #8b8b8b;
+  font-size: 12rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-decoration-line: underline;
+`;
+
+const input = css`
+  display: block;
+  padding: 12px;
+  border: none;
+  border-radius: 20rem;
+  background: #d9d9d9;
+  color: #8b8b8b;
+  font-size: 12rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  width: 100%;
+  padding: 12rem;
+  box-sizing: border-box;
+`;
+
 export default function ConfigPage() {
   const { logout } = useAuth();
 
   const onStartCooling = useCallback(() => {
     startCooling(10);
   }, []);
+
+  const {
+    value: isDeviceModalOpen,
+    setTrue: openDeviceModal,
+    setFalse: closeDeviceModal,
+  } = useBoolean(false);
+
+  const deviceModalStep = useStep(2);
 
   return (
     <>
@@ -49,7 +124,7 @@ export default function ConfigPage() {
         <Card>
           <div css={connectionSectionTitle}>連携中のデバイスはありません</div>
           <div css={{ textAlign: "center", marginTop: "7rem" }}>
-            <Button onClick={() => {}}>連携する</Button>
+            <Button onClick={openDeviceModal}>連携する</Button>
           </div>
           <hr css={{ marginBlock: "12rem", marginInline: "4rem" }} />
           <div css={connectionSectionTitle}>
@@ -86,7 +161,109 @@ export default function ConfigPage() {
         <div css={{ marginTop: "20rem", textAlign: "right" }}>
           <Button onClick={logout}>ログアウト</Button>
         </div>
+        <div css={cardTitle}>デバッグ</div>
+        <Card>
+          <div
+            css={{
+              display: "grid",
+              gridTemplate: "1fr 1fr / 1fr 1fr",
+              gap: "20rem 37rem",
+            }}
+          >
+            <Button addCss={debugGridButton} onClick={() => toast.success("Hello!")}>
+              Toast success
+            </Button>
+          </div>
+        </Card>
+
       </div>
+      <Modal
+        isOpen={isDeviceModalOpen}
+        onClose={() => {
+          closeDeviceModal();
+          deviceModalStep.reset();
+        }}
+      >
+        {() => {
+          switch (deviceModalStep.step) {
+            case 0:
+              return (
+                <div>
+                  <div css={modalTitle}>デバイス連携</div>
+                  <div css={[modalDescription]}>
+                    シリアルコードを入力してください
+                  </div>
+                  <input
+                    type="text"
+                    css={[
+                      input,
+                      css`
+                        margin-top: 8rem;
+                      `,
+                    ]}
+                    placeholder="ここにシリアルコードをコピー"
+                  />
+                  <div css={modalHint}>
+                    シリアルコードの取得方法は<span css={link}>こちら</span>
+                    をご覧ください
+                  </div>
+                  <div
+                    css={css`
+                      display: flex;
+                      justify-content: space-between;
+                      margin-top: 68rem;
+                    `}
+                  >
+                    <Button
+                      addCss={css`
+                        background-color: #e0e3e9;
+                      `}
+                      onClick={closeDeviceModal}
+                    >
+                      キャンセル
+                    </Button>
+                    <Button
+                      addCss={css`
+                        background-color: #e0e3e9;
+                      `}
+                      onClick={deviceModalStep.nextStep}
+                    >
+                      完了
+                    </Button>
+                  </div>
+                </div>
+              );
+            case 1:
+              return (
+                <div>
+                  <div css={modalTitle}>デバイス連携</div>
+                  <div css={modalSuccess}>連携が完了しました！</div>
+                  <div
+                    css={css`
+                      display: flex;
+                      justify-content: flex-end;
+                      margin-top: 32rem;
+                    `}
+                  >
+                    <Button
+                      addCss={css`
+                        background-color: #e0e3e9;
+                      `}
+                      onClick={() => {
+                        closeDeviceModal();
+                        deviceModalStep.reset();
+                      }}
+                    >
+                      とじる
+                    </Button>
+                  </div>
+                </div>
+              );
+            default:
+              return "Fuck off";
+          }
+        }}
+      </Modal>
     </>
   );
 }
